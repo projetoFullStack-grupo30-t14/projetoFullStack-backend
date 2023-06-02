de2fc23831e64c5a82b26075a2ef176821d863ea
CToH10 <luis.ebn98@gmail.com>
2023-06-02 12:24:51 -0300

    refactor car module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
   imports: [CarsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
