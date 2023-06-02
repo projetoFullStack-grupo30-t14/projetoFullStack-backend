8bea7fc49019f4c6da31bec85a02df00cbdd07bf
CToH10 <luis.ebn98@gmail.com>
2023-06-02 11:19:40 -0300

    feat: add base dto

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,1 +1,63 @@
-export class CreateCarDto {}
+import { Transform } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDate,
+  IsIn,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class CreateCarDto {
+  @IsString()
+  @IsNotEmpty()
+  brand: string;
+
+  @IsString()
+  @IsNotEmpty()
+  model: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  year: Date;
+
+  @IsIn(['electric', 'gas', 'hybrid'])
+  @IsNotEmpty()
+  fuel: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  mileage: number;
+
+  @IsString()
+  @IsNotEmpty()
+  color: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  price_FIPE: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  cover_image: string;
+
+  @IsBoolean()
+  @Transform(({ value }: { value: boolean }) => true, {
+    groups: ['transform'],
+  })
+  is_active: boolean;
+
+  @IsDate()
+  @Transform(({ value }: { value: string }) => Date.now(), {
+    groups: ['transform'],
+  })
+  created_at: Date;
+}
