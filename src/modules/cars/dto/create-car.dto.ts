3e4ebf1e48f76fbf704fb3face06da3905c492fb
CToH10 <luis.ebn98@gmail.com>
2023-06-26 14:18:53 -0300

    feat: add more efficient mapping

--- src/modules/cars/dto/create-car.dto.ts
+++ src/modules/cars/dto/create-car.dto.ts
@@ -1,23 +1,19 @@
 import { Transform } from 'class-transformer';
 import { IsIn, IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
 const current = new Date().getFullYear();
 
 export class CreateCarDto {
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }: { value: string }) => value.toLowerCase())
   brand: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }: { value: string }) => value.toLowerCase())
   model: string;
 
-  @ApiProperty()
   @IsInt()
   @Max(current, {
     message: `Must not be greater than current year: ${current}`,
@@ -26,44 +22,36 @@ export class CreateCarDto {
   @IsNotEmpty()
   year: number;
 
-  @ApiProperty()
   @IsIn(['electric', 'flex', 'hybrid'])
   @IsNotEmpty()
   fuel: 'electric' | 'flex' | 'hybrid';
 
-  @ApiProperty()
   @IsInt()
   @IsNotEmpty()
   mileage: number;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }: { value: string }) => value.toLowerCase())
   color: string;
 
-  @ApiProperty({ type: Number })
   @IsInt()
   @IsNotEmpty()
   price: number;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   description: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   cover_image: string;
 
-  @ApiProperty({ type: [String] })
   @IsString({ each: true })
   car_gallery: string[];
 }
 
 export class GalleryDto {
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   image: string;
--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -10,51 +10,41 @@ import {
 } from 'class-validator';
 import { CreateAddressDto } from 'src/modules/addresses/dto/create-address.dto';
 import { Address } from 'src/modules/addresses/entities/address.entity';
-import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   cpf: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   phone: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   date_of_birth: string;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   description: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   @ValidateNested()
   @Type(() => CreateAddressDto)
   address: Address;
 
-  @ApiProperty()
   @IsBoolean()
   @IsNotEmpty()
   seller: boolean;
 
-  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
