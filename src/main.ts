8e9dc545c8d8df34f864aee41acd98bcb70eedf7
CToH10 <luis.ebn98@gmail.com>
2023-06-26 13:41:21 -0300

    feat: add swagger base

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
 async function bootstrap() {
@@ -13,6 +14,16 @@ async function bootstrap() {
       transformOptions: { groups: ['transform'] },
     }),
   );
+
+  const nestConfig = new DocumentBuilder()
+    .setTitle('MotorShop API')
+    .setDescription('The API developed to allow users to buy and sell cars')
+    .setVersion('1.0')
+    .addTag('cars')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, nestConfig);
+  SwaggerModule.setup('docs', app, document);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
