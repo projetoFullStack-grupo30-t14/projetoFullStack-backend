50769adb6461d7817c7b96f8ec05aeec06336ca0
CToH10 <luis.ebn98@gmail.com>
2023-06-26 14:10:03 -0300

    feat: add basic api properties

--- src/modules/addresses/dto/create-address.dto.ts
+++ src/modules/addresses/dto/create-address.dto.ts
@@ -1,26 +1,33 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAddressDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   cep: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   state: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   city: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   street: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   number: string;
 
+  @ApiProperty()
   @IsString()
   complement: string;
 }
